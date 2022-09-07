f6d37314afcf0ec3fb0799d4ce935afc0f82b5a4
花裤熊二 <404437516@qq.com>
2022-09-07 10:29:48 +0800

    初试vue3响应式

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { reactive } from 'vue'
+// 无法响应
+// const obj = {
+// a: 1,
+// b: 2
+// }
+// const add = () => {
+// obj['c'] = 1
+// }
+
+// 响应式
+
+const obj = reactive({
+a: 1,
+b: 2
+})
+const add = () => {
+obj['c'] = 1
+}
+
+</script>
+
+<template>
+  <div>
+    {{obj}}
+    <button @click="add">添加</button>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
